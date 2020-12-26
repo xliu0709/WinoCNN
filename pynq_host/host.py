97a42dbc4cd3362cc4fd014dde4524e73a1ec397
xliu79 <xliu79@illinois.edu>
2020-12-26 00:29:30 -0600

    not

--- pynq_host/host.py
+++ pynq_host/host.py
@@ -514,7 +514,7 @@ if __name__ == "__main__":
     for i in range( conv_desc.output_height):
         for j in range( conv_desc.output_width):
             for k in range( conv_desc.output_depth//8):
-                print(i,j,k, output_FM[ (i*conv_desc.output_depth/8+k*conv_desc.output_width+j)*8:(i*conv_desc.output_depth/8+k*conv_desc.output_width+j)*8+8 ])        
+                print(i,j,k, output_FM[ (i*conv_desc.output_depth//8+k*conv_desc.output_width+j)*8:(i*conv_desc.output_depth//8+k*conv_desc.output_width+j)*8+8 ])        
         
     # for i in range(len(output_load)):
     #     if output_load[i]!=output_FM[i]:
