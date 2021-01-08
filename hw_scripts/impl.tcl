3f5518d501c7425a8a6bec140d7624dbdb02f6a6
xliu79 <xliu79@illinois.edu>
2021-01-08 02:39:20 -0600

    for test

---
+++ hw_scripts/impl.tcl
@@ -0,0 +1,37 @@
+create_project RTL ./RTL -part xczu9eg-ffvb1156-2-e
+set_property board_part xilinx.com:zcu102:part0:3.3 [current_project]
+
+create_bd_design "design_1"
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.3 zynq_ultra_ps_e_0
+apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1" }  [get_bd_cells zynq_ultra_ps_e_0]
+set_property -dict [list CONFIG.PSU__USE__M_AXI_GP1 {0} CONFIG.PSU__USE__S_AXI_GP2 {1} CONFIG.PSU__USE__S_AXI_GP3 {1} CONFIG.PSU__USE__S_AXI_GP4 {1} CONFIG.PSU__USE__S_AXI_GP5 {1} CONFIG.PSU__USE__IRQ0 {0}] [get_bd_cells zynq_ultra_ps_e_0]
+
+
+set_property  ip_repo_paths  ./HLS [current_project]
+update_ip_catalog
+
+
+create_bd_cell -type ip -vlnv xilinx.com:hls:wino_systolic_top:1.0 wino_systolic_top_0
+
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {Auto} Clk_slave {Auto} Clk_xbar {Auto} Master {/zynq_ultra_ps_e_0/M_AXI_HPM0_FPD} Slave {/wino_systolic_top_0/s_axi_AXILiteS} ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}}  [get_bd_intf_pins wino_systolic_top_0/s_axi_AXILiteS]
+
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {Auto} Clk_xbar {Auto} Master {/wino_systolic_top_0/m_axi_input_DDR0} Slave {/zynq_ultra_ps_e_0/S_AXI_HP0_FPD} intc_ip {Auto} master_apm {0}}  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP0_FPD]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {Auto} Clk_xbar {Auto} Master {/wino_systolic_top_0/m_axi_input_DDR1} Slave {/zynq_ultra_ps_e_0/S_AXI_HP1_FPD} intc_ip {Auto} master_apm {0}}  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP1_FPD]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {Auto} Clk_xbar {Auto} Master {/wino_systolic_top_0/m_axi_output_DDR0} Slave {/zynq_ultra_ps_e_0/S_AXI_HP2_FPD} intc_ip {Auto} master_apm {0}}  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP2_FPD]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {Auto} Clk_xbar {Auto} Master {/wino_systolic_top_0/m_axi_output_DDR1} Slave {/zynq_ultra_ps_e_0/S_AXI_HP3_FPD} intc_ip {Auto} master_apm {0}}  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP3_FPD]
+
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_xbar {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Master {/wino_systolic_top_0/m_axi_weight_DDR0} Slave {/zynq_ultra_ps_e_0/S_AXI_HP0_FPD} intc_ip {/axi_smc} master_apm {0}}  [get_bd_intf_pins wino_systolic_top_0/m_axi_weight_DDR0]
+# apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_xbar {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Master {/wino_systolic_top_0/m_axi_weight_DDR1} Slave {/zynq_ultra_ps_e_0/S_AXI_HP1_FPD} intc_ip {/axi_smc} master_apm {0}}  [get_bd_intf_pins wino_systolic_top_0/m_axi_weight_DDR1]
+# apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_xbar {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Master {/wino_systolic_top_0/m_axi_weight_DDR2} Slave {/zynq_ultra_ps_e_0/S_AXI_HP2_FPD} intc_ip {/axi_smc} master_apm {0}}  [get_bd_intf_pins wino_systolic_top_0/m_axi_weight_DDR2]
+# apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_xbar {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Master {/wino_systolic_top_0/m_axi_weight_DDR3} Slave {/zynq_ultra_ps_e_0/S_AXI_HP3_FPD} intc_ip {/axi_smc} master_apm {0}}  [get_bd_intf_pins wino_systolic_top_0/m_axi_weight_DDR3]
+
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_slave {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Clk_xbar {/zynq_ultra_ps_e_0/pl_clk0 (99 MHz)} Master {/wino_systolic_top_0/m_axi_mem_params} Slave {/zynq_ultra_ps_e_0/S_AXI_HP2_FPD} intc_ip {/axi_smc_2} master_apm {0}}  [get_bd_intf_pins wino_systolic_top_0/m_axi_mem_params]
+
+
+make_wrapper -files [get_files ./RTL/RTL.srcs/sources_1/bd/design_1/design_1.bd] -top
+add_files -norecurse ./RTL/RTL.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+
+
+launch_runs impl_1 -to_step write_bitstream -jobs 16
+wait_on_run impl_1
---
+++ hw_scripts/script.tcl
@@ -0,0 +1,42 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project HLS
+set_top wino_systolic_top
+add_files src/wino_struct.h
+add_files src/wino_macro.h
+add_files src/wino_hw_config.h
+add_files src/wino.h
+add_files src/wino.cpp
+
+add_files -tb software/buffer.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb software/buffer.h -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb software/matrix_utility.hpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb software/param.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb software/param.h -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb testbench/single_main.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb software/softlayer.hpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/wino.h -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src_gold/wino_IO_gold.hpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src_gold/wino_cell_gold.hpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src_gold/wino_gold.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src_gold/wino_gold.h -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/wino_hw_config.h -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/wino_macro.h -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/wino_struct.h -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+
+open_solution "solution1"
+set_part {xczu9eg-ffvb1156-2-e}
+create_clock -period 4 -name default
+config_sdx -target none
+config_export -format ip_catalog -rtl verilog -vivado_optimization_level 2 -vivado_phys_opt place -vivado_report_level 0
+set_clock_uncertainty 12.5%
+csynth_design
+# cosim_design -trace_level all -argv {14 14 8 14 14 8 3 1 1 1 227 random random random dump_txt /home/xliu79/Research/2020/WinoCNN}
+
+
+#source "./winocnn/solution1/directives.tcl"
+
+export_design -rtl verilog -format ip_catalog
