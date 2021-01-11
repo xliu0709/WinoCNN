609023e9e207189a967f38423de0ef609ac39e88
xliu79 <xliu79@illinois.edu>
2021-01-11 01:19:20 -0600

    board sync

--- testbench/single_main.cpp
+++ testbench/single_main.cpp
@@ -254,15 +254,15 @@ int main(int argc, char** argv)
 
 
 
-//    fptr=fopen("weight.bin","w");
-//
-//   fwrite(linfo_vect[0].weightbuffers_hw[0],1,linfo_vect[0].weightbuffers_hw_size[0],fptr);
-//   fclose(fptr);
-//
-//   fptr=fopen("input.bin","w");
-//
-//   fwrite(fmap_dict["in"].buffers_hw[0], 16,CEIL_DIV(input_depth,8)*input_height*ALIGN(input_height,8),fptr);
-//   fclose(fptr);
+    fptr=fopen("weight.bin","w");
+
+    fwrite(linfo_vect[0].weightbuffers_hw[0],1,linfo_vect[0].weightbuffers_hw_size[0],fptr);
+    fclose(fptr);
+
+    fptr=fopen("input.bin","w");
+
+    fwrite(fmap_dict["in"].buffers_hw[0], 16,CEIL_DIV(input_depth,8)*input_height*ALIGN(input_height,8),fptr);
+    fclose(fptr);
 
     if(dump_method == "compare" || dump_method=="dump_txt")
     {
